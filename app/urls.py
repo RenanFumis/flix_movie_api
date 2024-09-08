9c7dd4c9a9c5d91fb44db58570644145719186ca
RenanFumis <renan-oliveira19@hotmail.com>
2024-09-08 13:15:56 -0300

    feat: Criando a rota do endpoint  no Urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenereCreateListView, GenreRetrieveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 
     
     
@@ -9,5 +10,8 @@ urlpatterns = [
     
     path('genres/', GenereCreateListView.as_view(), name='genre_create_list'),
     path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre_detail_view')
+
+    path('actors/', ActorCreateListView.as_view(), name='actor_create_list'),
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor_detail_view')
 
 ]
