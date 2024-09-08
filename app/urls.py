964680962f6b9c0f1ea6b8acd7a3dbca5e47034a
RenanFumis <renan-oliveira19@hotmail.com>
2024-09-08 15:55:24 -0300

    feat: Criei o endpoint do admin Actors

--- app/urls.py
+++ app/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('genres/', GenereCreateListView.as_view(), name='genre_create_list'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre_detail_view')
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre_detail_view'),
 
     path('actors/', ActorCreateListView.as_view(), name='actor_create_list'),
-    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor_detail_view')
+    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor_detail_view'),
 
 ]
